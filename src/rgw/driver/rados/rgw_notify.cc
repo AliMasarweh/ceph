0c5bbf024bfe9661ece89aa95a225d6f0cd8be7a
Ali Masarwa <ali.saed.masarwa@gmail.com>
2023-08-17 17:46:46 +0300

    RGW | Bucket Notification: Fix the issue of miscounting the entries to remove
    Signed-off-by: Ali Masarwa <ali.saed.masarwa@gmail.com>

--- src/rgw/driver/rados/rgw_notify.cc
+++ src/rgw/driver/rados/rgw_notify.cc
@@ -305,7 +305,6 @@ private:
       is_idle = false;
       auto has_error = false;
       auto remove_entries = false;
-      uint64_t entries_to_remove = 0;
       auto entry_idx = 1U;
       tokens_waiter waiter(io_context);
       for (auto& entry : entries) {
@@ -314,13 +313,13 @@ private:
           break;
         }
         // TODO pass entry pointer instead of by-value
-        spawn::spawn(yield, [this, &queue_name, entry_idx, total_entries, &end_marker, &remove_entries, &entries_to_remove, &has_error, &waiter, entry](yield_context yield) {
+        spawn::spawn(yield, [this, &queue_name, entry_idx, total_entries, &end_marker, &remove_entries, &has_error, &waiter, entry](yield_context yield) {
+
             const auto token = waiter.make_token();
             if (process_entry(entry, yield)) {
               ldpp_dout(this, 20) << "INFO: processing of entry: " << 
                 entry.marker << " (" << entry_idx << "/" << total_entries << ") from: " << queue_name << " ok" << dendl;
               remove_entries = true;
-              ++entries_to_remove;
             }  else {
               if (set_min_marker(end_marker, entry.marker) < 0) {
                 ldpp_dout(this, 1) << "ERROR: cannot determin minimum between malformed markers: " << end_marker << ", " << entry.marker << dendl;
@@ -340,6 +339,14 @@ private:
 
       // delete all published entries from queue
       if (remove_entries) {
+        uint64_t entries_to_remove = 0;
+        for (const auto& entry: entries) {
+          if (end_marker == entry.marker) {
+            break;
+          }
+          entries_to_remove++;
+        }
+
         librados::ObjectWriteOperation op;
         op.assert_exists();
         rados::cls::lock::assert_locked(&op, queue_name+"_lock", 
