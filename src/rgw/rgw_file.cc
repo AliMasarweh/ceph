699a8bab1aa180f84027ca8a6341b57fa3fcf0c3
Ali Maredia <amaredia@redhat.com>
2023-10-05 18:57:27 +0000

    SQUASH-ME: add CountersContainer to RGWWriter
    Signed-off-by: Ali Maredia <amaredia@redhat.com>

--- src/rgw/rgw_file.cc
+++ src/rgw/rgw_file.cc
@@ -1838,7 +1838,7 @@ namespace rgw {
     ceph_assert(! dlo_manifest);
     ceph_assert(! slo_info);
 
-    auto counters = rgw::op_counters::get(state);
+    counters = rgw::op_counters::get(state);
     rgw::op_counters::inc(counters, l_rgw_op_put, 1);
     op_ret = -EINVAL;
 
@@ -1945,7 +1945,6 @@ namespace rgw {
     real_time appx_t = real_clock::now();
 
     state->obj_size = bytes_written;
-    auto counters = rgw::op_counters::get(state);
     rgw::op_counters::inc(counters, l_rgw_op_put_b, state->obj_size);
 
     // flush data in filters
@@ -2029,7 +2028,6 @@ namespace rgw {
     }
 
   done:
-    counters = rgw::op_counters::get(state);
     rgw::op_counters::tinc(counters, l_rgw_op_put_lat, state->time_elapsed());
     return op_ret;
   } /* exec_finish */
