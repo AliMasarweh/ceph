1cd95edb3b4c2e5a739296a8db4023d5f5f8f7aa
Ali Masarwa <amasarwa@redhat.com>
2025-04-08 17:17:29 +0300

    FIX
    Signed-off-by: Ali Masarwa <amasarwa@redhat.com>

--- src/rgw/rgw_realm.cc
+++ src/rgw/rgw_realm.cc
@@ -73,7 +73,7 @@ int RGWRealm::find_zone(const DoutPrefixProvider *dpp,
   epoch_t epoch = 0;
 
   RGWPeriod period(period_id, epoch);
-  int r = cfgstore->read_period(dpp, y, period_id, epoch, period);
+  int r = cfgstore->read_period(dpp, y, current_period, std::nullopt, period);
   if (r < 0) {
     ldpp_dout(dpp, 0) << "WARNING: period init failed: " << cpp_strerror(-r) << " ... skipping" << dendl;
     return r;
